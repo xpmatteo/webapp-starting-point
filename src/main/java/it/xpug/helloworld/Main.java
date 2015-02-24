e0a164e8535c508463c07d08650e4e4824884403
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-02-24 15:26:01 +0100

    Fixed heroku support

--- src/main/java/it/xpug/helloworld/Main.java
+++ src/main/java/it/xpug/helloworld/Main.java
@@ -1,5 +1,6 @@
 package it.xpug.helloworld;
 
+import static java.lang.Integer.valueOf;
 import it.xpug.toolkit.db.Database;
 import it.xpug.toolkit.db.DatabaseConfiguration;
 import it.xpug.toolkit.http.ReusableJettyApp;
@@ -7,18 +8,28 @@ import it.xpug.toolkit.http.ReusableJettyApp;
 
 public class Main {
 	private static final String DEVELOPMENT_DATABASE_URL = "postgres://myproject:secret@localhost:5432/myproject_development";
+	private static final String DEFAULT_PORT = "8080";
 	
 	public static void main(String[] args) {
 		DatabaseConfiguration configuration = new DatabaseConfiguration(getDatabaseUrl());
 		Database database = new Database(configuration);
-		ReusableJettyApp app = new ReusableJettyApp(new HelloWorldServlet(database));
-		app.start(8080, "src/main/webapp");
+		HelloWorldServlet servlet = new HelloWorldServlet(database);
+		ReusableJettyApp app = new ReusableJettyApp(servlet);
+		app.start(getPort(), "src/main/webapp");
+	}
+	
+	private static int getPort() {
+		return valueOf(getenv("PORT", DEFAULT_PORT));
 	}
 
 	private static String getDatabaseUrl() {
-		String databaseUrl = System.getenv("DATABASE_URL");
-		if (null == databaseUrl) 
-			databaseUrl = DEVELOPMENT_DATABASE_URL;
-		return databaseUrl;
+		return getenv("DATABASE_URL", DEVELOPMENT_DATABASE_URL);
+	}
+
+	private static String getenv(String key, String defaultValue) {
+		String value = System.getenv(key);
+		if (null == value)
+			return defaultValue;
+		return value;
 	}
 }
