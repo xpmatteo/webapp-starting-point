776bd97e83a0fb4f700b2e319a771fac61f89b4a
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-02-24 15:09:18 +0100

    Add heroku support

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,7 +6,11 @@
 #  0. register yourself on Heroku
 #  1. create a new application on Heroku
 #  2. install the "heroku toolbelt" (see instructions on heroku's website)
-#  3. execute 'git remote create heroku XXX' where XXX is the url of
-#     your Heroku application
+#  3. write the name of the Main class you want to use in the Procfile
+#  3. execute 
+#        heroku login
+#        heroku git:remote -a <yourappname>
 
 git push heroku master
+
+# Then open http://<yourappname>.herokuapp.com
