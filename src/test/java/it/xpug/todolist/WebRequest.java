8c0f23eefafe31654f41fc365f9b3c6695ae562e
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-12-29 11:55:14 +0100

    Add WebRequest

---
+++ src/test/java/it/xpug/todolist/WebRequest.java
@@ -0,0 +1,25 @@
+package it.xpug.todolist;
+
+import java.util.regex.*;
+
+import javax.servlet.http.*;
+
+public class WebRequest {
+
+	private HttpServletRequest httpServletRequest;
+	private Matcher matcher;
+
+	public WebRequest(HttpServletRequest anHttpServletRequest) {
+		this.httpServletRequest = anHttpServletRequest;
+    }
+
+	public boolean matches(String regex) {
+		matcher = Pattern.compile(regex).matcher(httpServletRequest.getRequestURI());
+		return matcher.matches();
+    }
+
+	public String getUriParameter(int index) {
+	    return matcher.group(index);
+    }
+
+}
---
+++ src/test/java/it/xpug/todolist/WebRequestTest.java
@@ -0,0 +1,27 @@
+package it.xpug.todolist;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import javax.servlet.http.*;
+
+import org.junit.*;
+
+public class WebRequestTest {
+	HttpServletRequest anHttpServletRequest = mock(HttpServletRequest.class);
+	WebRequest webRequest = new WebRequest(anHttpServletRequest);
+
+	@Test
+    public void matchParameters() throws Exception {
+		when(anHttpServletRequest.getRequestURI()).thenReturn("/foo/1234");
+		assertTrue("matches pattern", webRequest.matches("/foo/(\\d+)"));
+		assertEquals("1234", webRequest.getUriParameter(1));
+    }
+
+	@Test
+    public void doesNotMatchParameters() throws Exception {
+		when(anHttpServletRequest.getRequestURI()).thenReturn("something else");
+		assertFalse("should not match pattern", webRequest.matches("/foo/(\\d+)"));
+    }
+
+}
