17524a7f9d5277686907bbed17cc28d66ad2d793
Matteo Vaccari <mvaccari@thoughtworks.com>
2016-01-11 19:50:48 +0100

    Add find capability to EventStore

--- src/main/java/it/xpug/todolist/EventStore.java
+++ src/main/java/it/xpug/todolist/EventStore.java
@@ -1,9 +1,14 @@
 package it.xpug.todolist;
 
+import static java.lang.Class.*;
+
+import java.util.*;
+
 import it.xpug.toolkit.db.*;
 
 import org.json.*;
 
+import com.google.gson.*;
 import com.saasovation.common.domain.model.*;
 
 public class EventStore implements DomainEventSubscriber<DomainEvent> {
@@ -30,5 +35,25 @@ public class EventStore implements DomainEventSubscriber<DomainEvent> {
 	@Override
     public Class<DomainEvent> subscribedToEventType() {
 	    return DomainEvent.class;
+    }
+
+	public List<DomainEvent> findEvents(String entityId) {
+		String sql = "select * from domain_events where entityId = ? order by id asc";
+		ListOfRows rows = database.select(sql, entityId);
+		if (0 == rows.size())
+			return null;
+
+		List<DomainEvent> events = new ArrayList<>();
+		for (Map<String, Object> row : rows.toCollection()) {
+			try {
+				Class<?> klass = forName("it.xpug.todolist." + row.get("eventtype"));
+				String json = (String) row.get("params").toString();
+				Object event = new Gson().fromJson(json, klass);
+                events.add((DomainEvent) event);
+            } catch (JsonSyntaxException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+		return events;
     }
 }
--- src/test/java/it/xpug/todolist/EventStoreTest.java
+++ src/test/java/it/xpug/todolist/EventStoreTest.java
@@ -1,5 +1,6 @@
 package it.xpug.todolist;
 
+import static java.util.Arrays.*;
 import static org.junit.Assert.*;
 
 import java.util.*;
@@ -7,6 +8,8 @@ import java.util.*;
 import it.xpug.toolkit.db.*;
 
 import org.junit.*;
+
+import com.saasovation.common.domain.model.*;
 
 public class EventStoreTest {
 
@@ -29,6 +32,20 @@ public class EventStoreTest {
 	    assertEquals("TodoListCreatedEvent", row.get("eventtype"));
 	    assertEquals("{\"name\":\"a list\",\"id\":\"333\"}", row.get("params").toString());
     }
+
+	@Test
+    public void findEvents() throws Exception {
+	    TodoListCreatedEvent a = new TodoListCreatedEvent("333", "a list");
+	    TodoListCreatedEvent b = new TodoListCreatedEvent("444", "a list");
+	    TodoListRenamedEvent c = new TodoListRenamedEvent("333", "other name");
+		eventStore.handleEvent(a);
+		eventStore.handleEvent(b);
+		eventStore.handleEvent(c);
+
+	    List<DomainEvent> events = eventStore.findEvents("333");
+		assertEquals(asList(a, c), events);
+    }
+
 
 	@Test
     public void renamedEventDatabaseRow() throws Exception {
